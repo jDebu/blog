42ab7e2ed871afb473d0be7ddf950eb8a98a3184
manuel delgado <mdelgado.pcm@gmail.com>
2024-02-12 03:55:13 -0500

    build: modify environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,6 +74,7 @@ Rails.application.configure do
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.hosts << "jdebu.dev"
+  config.action_mailer.default_url_options = { host: 'jdebu.dev' }
   Rails.application.routes.default_url_options = config.action_mailer.default_url_options
 end
